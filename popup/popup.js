9f6858b6cd6e34a8fba42d1d6ea3c50522a8c813
tac1DEV <sansberrot@gmail.com>
2026-04-29 16:31:50 +0200

    style: match

--- popup/popup.js
+++ popup/popup.js
@@ -9,7 +9,28 @@ connect.addEventListener("submit", async (e) => {
   const tagline = inputValue.split("-")[1];
   const data =  await browser.runtime.sendMessage({ type: "GET_PLAYER", summonerName, tagline });
   console.log("data", data);
-  document.getElementById("historique").textContent = JSON.stringify(data, null, 2);
+  if(data) {
+    connect.classList.add("hidden");
+    for(let i = 0; i < 5; i++) {
+      const champIcon = document.createElement("img");
+      champIcon.src = data.champIcon;
+      const score = document.createElement("p");
+      score.style.color = "#ffffff";
+      score.textContent = data.score;
+      const result = document.createElement("p");
+      result.textContent = data.win ? "WIN" : "LOSS";
+      result.style.color = data.win ? "#00c7bd" : "#ff7070";
+      const grid = document.getElementById("grid");
+      const historiqueDiv = document.createElement("div");
+      historiqueDiv.classList.add("historique");
+      historiqueDiv.style.backgroundColor = data.win ? "#4767ac" : "#974545";
+      historiqueDiv.style.border = data.win ? "5px solid #252c55" : "5px solid #722929";
+      historiqueDiv.appendChild(champIcon);
+      historiqueDiv.appendChild(result);
+      historiqueDiv.appendChild(score);
+      grid.appendChild(historiqueDiv);    
+    }
+  }
 });
 }
 
