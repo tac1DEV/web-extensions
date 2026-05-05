735c5c159a21571bc1e41bfd7f98ef6391f2c388
tac1DEV <sansberrot@gmail.com>
2026-05-05 15:20:27 +0200

    utilisation du cache pour afficher les données

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 const API_URL = "https://europe.api.riotgames.com";
 
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 browser.runtime.onMessage.addListener(async (msg) => {
   if (msg.type === "GET_PLAYER") {
     const { API_KEY } = await import("./config.js");
@@ -8,18 +12,20 @@ browser.runtime.onMessage.addListener(async (msg) => {
     const matchIDs = await fetch(API_URL + "/lol/match/v5/matches/by-puuid/" + data.puuid + "/ids?api_key=" + API_KEY);
     const matchIDsData = await matchIDs.json();
     const matchDataArray = [];
-      for (const matchID of matchIDsData.slice(0, 5)) { 
+      for (const matchID of matchIDsData.slice(0, 10)) { 
       const match = await fetch(API_URL + "/lol/match/v5/matches/" + matchID + "?api_key=" + API_KEY);
       const matchData = await match.json();
       const participants = await matchData.info.participants;
-      const index = participants.findIndex(participant => participant.riotIdGameName?.toLowerCase() === msg.summonerName.toLowerCase() && participant.riotIdTagline?.toLowerCase() === msg.tagline.toLowerCase());
+      const index = participants.findIndex(participant => participant.riotIdGameName?.replace(/\s/g, '').toLowerCase() === msg.summonerName.replace(/\s/g, '').toLowerCase() && participant.riotIdTagline?.replace(/\s/g, '').toLowerCase() === msg.tagline.replace(/\s/g, '').toLowerCase());
       const dataGame = {
         champIcon: "https://ddragon.leagueoflegends.com/cdn/16.8.1/img/champion/"+participants[index].championName+".png",
         score: participants[index].kills+"/"+participants[index].deaths+"/"+participants[index].assists,
         win: participants[index].win // win ou loose
       };
       matchDataArray.push(dataGame);
+      await sleep(50);
       }
+    console.log(matchDataArray);
     return matchDataArray;
   }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,54 +1,84 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
   const summoner = await loadSummoner();
+  console.log("Popup chargé", summoner);
   if (summoner) {
     document.getElementById("summonerName").value = `${summoner.summonerName}-${summoner.tagline}`;
-  }
-  connection();
+    const stored = await browser.storage.local.get("matchHistory");
+    displayData(stored.matchHistory); 
+  };
 });
 
-function connection() {
+function displayData(data) {
+  const grid = document.getElementById("grid");
 
+  //reset le contenu de grid
+  grid.innerHTML = "";
+  grid.style.display = "grid";
+  grid.style.height = "100%";
+  grid.style.overflowX = "scroll";
+
+  data.forEach((match) => {
+    const champIcon = document.createElement("img");
+    const score = document.createElement("p");
+    const result = document.createElement("p");
+    const historiqueDiv = document.createElement("div");
+
+    champIcon.src = match.champIcon;
+    champIcon.style.border = match.win ? "1px solid blue" : "1px solid red";
+
+    result.textContent = match.win ? "WIN" : "LOSS";
+    result.style.color = match.win ? "#00c7bd" : "#ff7070";
+
+    score.textContent = match.score;
+    score.style.color = "#ffffff";
+
+    historiqueDiv.classList.add("historique");
+    historiqueDiv.style.backgroundColor = match.win ? "#4767ac" : "#974545";
+    historiqueDiv.style.border = match.win
+      ? "5px solid #252c55"
+      : "5px solid #722929";
+
+    historiqueDiv.appendChild(champIcon);
+    historiqueDiv.appendChild(result);
+    historiqueDiv.appendChild(score);
+
+    grid.appendChild(historiqueDiv);
+  });
+}
+
+async function getData(summonerName, tagline) {
+  const data = await browser.runtime.sendMessage({
+    type: "GET_PLAYER",
+    summonerName,
+    tagline
+  });
+
+  if (data) {
+    displayData(data);
+
+    await browser.storage.local.set({
+      matchHistory: data
+    });
+
+  } else {
+    return reportExecuteScriptError(
+      new Error("Aucun résultat trouvé pour ce joueur.")
+    );
+  }
+}
+
+function connection() {
 const connect = document.getElementById("connect");
 
 connect.addEventListener("submit", async (e) => {
   e.preventDefault();
+console.log(loadSummoner());
   const inputValue = document.getElementById("summonerName").value;
   const summonerName = inputValue.split("-")[0];
   const tagline = inputValue.split("-")[1];
   await saveSummoner(summonerName, tagline);
-  const data =  await browser.runtime.sendMessage({ type: "GET_PLAYER", summonerName, tagline });
-  if(data) {
-    const grid = document.getElementById("grid");
-    grid.style.display = "grid";
-    grid.style.height = "100%";
-    grid.style.overflowX = "scroll";
-    data.forEach((match) => {
-      const champIcon = document.createElement("img");
-      const score = document.createElement("p");
-      const result = document.createElement("p");
-      const historiqueDiv = document.createElement("div");
-
-      champIcon.src = match.champIcon;
-      champIcon.style.border = match.win ? "1px solid blue" : "1px solid red";
-      historiqueDiv.appendChild(champIcon);
-
-      result.textContent = match.win ? "WIN" : "LOSS";
-      result.style.color = match.win ? "#00c7bd" : "#ff7070";
-      historiqueDiv.appendChild(result);
-
-      score.style.color = "#ffffff";
-      score.textContent = match.score;
-      historiqueDiv.appendChild(score);
-
-      historiqueDiv.classList.add("historique");
-      historiqueDiv.style.backgroundColor = match.win ? "#4767ac" : "#974545";
-      historiqueDiv.style.border = match.win ? "5px solid #252c55" : "5px solid #722929";
-      grid.appendChild(historiqueDiv);  
-    });
-  }else{
-    return reportExecuteScriptError(new Error("Aucun résultat trouvé pour ce joueur."));
-  }
+  await getData(summonerName, tagline );
 });
 }
 
